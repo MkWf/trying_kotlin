10d8fe856e55b7d48d573b9f2c118051558ae9b7
Mark Wassef <markdevw@gmail.com>
2015-10-30 17:34:11 -0400

    Added toolbar and recyclerview

--- app/src/main/kotlin/com/gmail/markdevw/tryingkotlin/activities/MainActivity.kt
+++ app/src/main/kotlin/com/gmail/markdevw/tryingkotlin/activities/MainActivity.kt
@@ -2,6 +2,8 @@ package com.gmail.markdevw.tryingkotlin.activities
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.widget.RecyclerView
+import android.support.v7.widget.Toolbar
 
 import com.gmail.markdevw.tryingkotlin.R
 
@@ -10,7 +12,8 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-    }
 
-    
+        val mToolbar = findViewById(R.id.tb_activity_main) as Toolbar
+        val mRecyclerView = findViewById(R.id.rv_activity_main) as RecyclerView
+    }
 }
